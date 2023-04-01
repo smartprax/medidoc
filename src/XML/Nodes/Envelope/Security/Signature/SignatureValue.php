722edb2922e70e9bd446c4500673fe48b48a5df4
Rico Leuthold <rico.leuthold@smartcode.ch>
2023-04-01 10:33:36 +0200

    Signature value is calculated correctly.

--- src/XML/Nodes/Envelope/Security/Signature/SignatureValue.php
+++ src/XML/Nodes/Envelope/Security/Signature/SignatureValue.php
@@ -2,18 +2,16 @@
 
 namespace Smartprax\Medidoc\XML\Nodes\Envelope\Security\Signature;
 
-use Sabre\Xml\Writer;
-use Smartprax\Medidoc\Methods\Method;
-use Smartprax\Medidoc\XML\Nodes\Envelope\Security\Timestamp;
+use Smartprax\Medidoc\Methods\Login;
 use Smartprax\Medidoc\XML\Nodes\Node;
 use Smartprax\Medidoc\XML\XML_NS;
 
 class SignatureValue extends Node
 {
 
-    public function __construct(protected Method $method) {}
+    public function __construct(protected Login $login, protected SignedInfo $signedInfo) {}
 
-    public function namespace(): ?XML_NS
+    public static function namespace(): ?XML_NS
     {
         return null;
     }
@@ -25,34 +23,50 @@ class SignatureValue extends Node
 
     public function value(): string|array
     {
+        $signed_info   = $this->signedInfo->xml(); // checked.
+        $client_secret = $this->login->clientSecret(); // checked.
+        $binary_secret = $this->login->binarySecret();
 
-        $writer = new Writer();
-        $timestamp = new SignedInfo($this->method);
+        //$signed_info = '<SignedInfo xmlns="http://www.w3.org/2000/09/xmldsig#"><CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"></CanonicalizationMethod><SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#hmac-sha1"></SignatureMethod><Reference URI="#_0"><Transforms><Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"></Transform></Transforms><DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"></DigestMethod><DigestValue>A/IMXF7+3wLzotqI/a2uQQs5Pk0=</DigestValue></Reference></SignedInfo>';
+        //$client_secret = 'oXbz6G05K29efuAGDEDpj4yOMcU=';
+        //$binary_secret = ' 6NLbwiNS96UcdK9exi8b/2+kBRLafsX8WcQHF1Zh16Y=';
 
-        $writer->write([
-            'name' => $timestamp->name(),
-            'attributes' => [
-                ...$timestamp->attributes(),
-                'xmlns' => 'http://www.w3.org/2000/09/xmldsig#',
-            ],
-            'value' => $timestamp->value(),
-        ]);
+        //ray('Signed info:' . $signed_info, 'Client secret: '. $client_secret, 'Server Binary Secret: ' . $binary_secret);
 
-        $timestamp_xml = $writer->outputMemory();
+        $psha_1_secret = static::psha1(\base64_decode($client_secret), \base64_decode($binary_secret));
+        $sha_1_hmac = hash_hmac("sha1", $signed_info, $psha_1_secret, true);
 
-        $client_secret = base64_encode(
-            pack('H*',
-                sha1(
-                    pack('H*', \mt_rand()) .
-                    pack('a*', (new Timestamp($this->method))->created()) .
-                    pack('a*', \config('medidoc.encryption_key')))
-            )
-        );
+        //ray('Signature Value:' . base64_encode($sha_1_hmac));
 
-        $binServerSecret= base64_decode($serverSecret);
-        $psha1Secret    = self::psha1($client_secret, $binServerSecret);
-        $signatuHmac    = hash_hmac("sha1", $timestamp_xml, $psha1Secret, true);
+        return base64_encode($sha_1_hmac);
+    }
 
-        return base64_encode($signatuHmac);
+    private static function psha1($clientSecret, $serverSecret) : string
+    {
+        $sizeBytes = 256 / 8;
+
+        $hmacKey = $clientSecret;
+        $i = 0;
+
+        $b1 = $serverSecret;
+        $psha = [];
+
+        while ($i < $sizeBytes) {
+
+            $b1 = hash_hmac('SHA1', $b1, $hmacKey, true);
+            $b2 = $b1 . $serverSecret;
+            $temp = hash_hmac('SHA1', $b2, $hmacKey, true);
+
+            for ($j = 0; $j < strlen($temp); $j++) {
+                if ($i < $sizeBytes) {
+                    $psha[$i] = $temp[$j];
+                    $i++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return implode("", $psha);
     }
 }
