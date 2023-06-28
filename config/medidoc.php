78faf73dbf6ab7bc96597d71c754347c82a5bbc8
Rico Leuthold <rico.leuthold@smartcode.ch>
2023-06-28 16:29:24 +0200

    Add prod and test endpoints to config. Set prod endpoint as default.

--- config/medidoc.php
+++ config/medidoc.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 return [
-    'endpoint' => env('MEDIDOC_ENDPOINT', 'https://test.medidoc.ch/webservices/MedidocSoapAccessV2.asmx?WSDL'),
+    # Production
+    'endpoint' => env('MEDIDOC_ENDPOINT', 'https://www.medidoc.ch/webservices/MedidocSoapAccessV2.asmx?WSDL'),
+    # Test
+    #'endpoint' => env('MEDIDOC_ENDPOINT', 'https://test.medidoc.ch/webservices/MedidocSoapAccessV2.asmx?WSDL'),
     'gln' => env('MEDIDOC_GLN'),
     'password' => env('MEDIDOC_PASSWORD'),
 ];
