b1890112aa0fcb10e6d841e0949f7d2f40edb664
Rico Leuthold <rico.leuthold@smartcode.ch>
2023-07-20 12:41:43 +0200

    Clean up.

--- tests/DocumentTests.php
+++ tests/DocumentTests.php
@@ -80,8 +80,6 @@ test('GetDocumentResponseContent', function (string $DocumentGID) {
 
     $response = GetDocumentContent::run($DocumentGID, false);
 
-    ray()->xml($response->DocumentContent);
-
     expect($response)
         ->toBeInstanceOf(ContentResponse::class);
 
